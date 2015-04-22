25344165b54516909d4a18e35d9448bafa3f6f0a
jefersonla <oi_je@hotmail.com>
2015-04-22 20:13:36 -0300

    Atualizado alguns detalhes

--- TATUDevice/TATUDevice.h
+++ TATUDevice/TATUDevice.h
@@ -2,47 +2,27 @@
 #define TATUInterpreter_h
 
 #include "hash_list.h"
+#include <TATUInterpreter.h>
 #include <stdint.h>
 
-#define TATU_SET    0
-#define TATU_GET    1
-#define TATU_POST   2
-#define TATU_EDIT   3   
-
-#define TATU_CODE_ALL   0
-#define TATU_CODE_INFO  1
-#define TATU_CODE_PROP  2
-#define TATU_CODE_STATE 3
-
-#define CODE_ALL   'A'
-#define CODE_INFO  'I'
-#define CODE_PROP  'P'
-#define CODE_STATE 'S'
-
-#define COMMAND_SET  'S'
-#define COMMAND_GET  'G'
-#define COMMAND_POST 'P'
-#define COMMAND_EDIT 'E'
-
+#ifndef PIN_AMOUNT
 #define PIN_AMOUNT 6
-
- //hash
-
+#endif
 
 #define HASH_DJB(START, LEN, INPUT, OUTPUT) for(i = START; i < LEN; i++){ OUTPUT = ((OUTPUT << 5) + OUTPUT) + INPUT[i]; }
 
-//Calcula o comprimento da string
+// Calcula o comprimento da string
 #define STRLEN(STRING,LENGTH) for(LENGTH = 0; STRING[LENGTH] != '\0';){LENGTH++;}
 
-//Concatena duas strings
+// Concatena duas strings
 #define CONCAT(FIRST,SECOND,LENGTH) for(i = 0; SECOND[i] != '\0'; FIRST[LENGTH] = SECOND[i],i++,LENGTH++); FIRST[LENGTH] = '\0'
-//Concatena uma string e um char
+// Concatena uma string e um char
 #define CONCATSINGLE(FIRST,SECOND,LENGTH) if(true) { FIRST[LENGTH++] = (char)SECOND; FIRST[LENGTH + 1] = '\0'; } 
-//concatena um objeto com um elemento adicional
+// Concatena um objeto com um elemento adicional
 #define CONCATELEMENT(FIRST,SECOND,LENGTH) if(true) {CONCATSINGLE(FIRST,',',LENGTH);CONCAT(FIRST,SECOND,LENGTH);}
-//concatena uma variavel com o seu valor
+// Concatena uma variavel com o seu valor
 #define CONCATVALUE(FIRST,SECOND,LENGTH) if(true) {CONCATSINGLE(FIRST,':',LENGTH);CONCAT(FIRST,SECOND,LENGTH);}
-//concatena três objetos na ordema passada
+// Concatena três objetos na ordema passada
 #define CONCATTHREE(FIRST,SECOND,THIRD,LENGTH) if(true) {CONCAT(FIRST,SECOND,LENGTH);CONCAT(SECOND,THIRD,LENGTH);}
 
 //DEPRECATED FOR NOW
@@ -56,7 +36,6 @@
 //Calcula o comprimento da string
 class Device{
 private:
-
     typedef union {
         struct {
             uint8_t STATE : 1;
@@ -66,30 +45,27 @@ private:
         } OBJ;
         uint8_t STRUCTURE;
     } Command;
-    //informções da EEPROM 
-    
-
-    
 public:
+    uint8_t DEVICE_ID;
+    char DEVICE_NAME[21];
     uint8_t PAN_ID;
-    uint8_t RESET_TIMES;
     uint16_t START_TIME;
+    uint8_t RESET_TIMES;
     uint8_t SAMPLE_RATE;
     uint8_t OS_VERSION;
-    uint8_t DEVICE_ID;
+    
     uint8_t pins_value[PIN_AMOUNT];
     char pins_alias[PIN_AMOUNT][6];
-    char DEVICE_NAME[21];
     
     uint16_t MQTT_PORT;
     char DEVICE_IP[16];
     char MQTT_IP[16];
-    //char *room;
-
+    
     Command cmd;
     char msg[200] = DEVICE_NAME;
-    void post();
-    void setHeader();
+    
+    void generateHeader();
+    void generatePost();
 };
 
 #endif
