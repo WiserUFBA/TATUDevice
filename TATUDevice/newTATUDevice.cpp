8879df373c7fc2f8816096a7966d4b7826be475c
jefersonla <oi_je@hotmail.com>
2015-04-23 15:23:00 -0300

    Feitas modificações teste inicial pronto para o body null

--- TATUDevice/newTATUDevice.cpp
+++ TATUDevice/newTATUDevice.cpp
@@ -1,4 +1,17 @@
+#include "TATUDevice.h"
 #include <stdint.h>
+#include <string.h>
+#include <avr/pgmspace.h>
+
+// Constantes
+FLASH_DECLARE(char start_post = "POST ");
+FLASH_DECLARE(char null_body = "\"BODY\":null}");
+FLASH_DECLARE(char header_str = "\"HEADER\":{");
+FLASH_DECLARE(char name_str = "\"NAME\":\"");
+FLASH_DECLARE(char id_str = "\"ID\":");
+FLASH_DECLARE(char pan_str = "\"PAN\":");
+FLASH_DECLARE(char ip_str = "\"IP\":\"");
+FLASH_DECLARE(char body_str = "\"BODY\":{");
 
 TATUDevice::TATUDevice( const char *name_d,     const char *ip_d, const uint8_t id_d,    const uint8_t pan_d,
                         const uint8_t sample_d, const char *ip_m, const uint16_t port_m, const uint8_t os_v){
@@ -18,6 +31,89 @@ TATUDevice::TATUDevice( const char *name_d,     const char *ip_d, const uint8_t 
     generateHeader();
 }
 
-TATUDevice::generateHeader(){
-    // Primeiro se coloca a seguinte string padrão no vetor
+TATUDevice::put_braces(char *brace_place, bool direction){
+    *brace_place = direction ? '{' : '}'; 
 }
+
+TATUDevice::put_colon(char *colon_place, bool string){
+    colon_place[0] = ':';
+    if(string) colon_place[1] = '\"';
+}
+
+TATUDevice::put_comma(char *comma_place, bool string){
+    if(string) comma_place[0] = '\"';
+    colon_place[1] = ','
+}
+
+TATUDevice::put_colon_braces(char *brace_place){
+    brace_place[0] = ':';
+    brace_place[1] = '{'; 
+}
+
+TATUDevice::generateHeader(){
+    /* Auxiliary variable */
+    int aux;
+    char aux_str[10];
+
+    // Primeiro se coloca a seguinte string padrão no vetor
+    strcpy_P(output_message, start_post);
+    strcpy(&output_message[5], device_name);
+    
+    // Inicia o JSON
+    aux = strlen(device_name) + 5;
+    put_colon_braces(&output_message[aux]);
+    aux += 2;
+    
+    // As próximas linhas produzem o HEADER
+    strcpy_P(&output_message[aux], header_str); /* Copia o HEADER */
+    aux += 10;
+    
+    /* Coloca o NAME */
+    strcpy_P(&output_message[aux], name_str);
+    aux += 8;
+    strcpy(&output_message[aux], device_name);
+    aux += strlen(device_name);
+    put_comma(&output_message[aux], ISSTRING);
+    aux += 2;
+
+    /* Coloca o ID */
+    strcpy_P(&output_message[aux], id_str);
+    aux += 5;
+    itoa(device_id, aux_str, 10);
+    strcpy(&output_message[aux], aux_str);
+    aux += strlen(aux_str);
+    put_comma(&output_message[aux], ISNOTSTRING);
+    aux += 1;
+    
+    /* Coloca o PAN */
+    strcpy_P(&output_message[aux], pan_str);
+    aux += 6;
+    itoa(device_pan, aux_str, 10);
+    strcpy(&output_message[aux], aux_str);
+    aux += strlen(aux_str);
+    put_comma(&output_message[aux], ISNOTSTRING);
+    aux += 1;
+    
+    /* Coloca o IP */
+    strcpy_P(&output_message[aux], ip_str);
+    aux += 6;
+    strcpy(&output_message[aux], device_ip);
+    aux += strlen(device_ip);
+    
+    /* Fecha a mensagem de saída */
+    output_message[aux++] = '\"';
+    output_message[aux++] = '}';
+    output_message[aux++] = ',';
+    output_message[aux] = 0;
+
+    last_char = aux;
+}
+
+TATUDevice::generateBody(bool success){
+    if(!success) strcpy_P(&output_message[last_char], null_body);
+    else{
+        
+    }
+}
+
+// 
--- TATUDevice/newTATUDevice.h
+++ TATUDevice/newTATUDevice.h
@@ -8,12 +8,24 @@
 #define PIN_AMOUNT 6
 #endif
 
+// Constantes do sistema
+#if defined(__ICCAVR__) // IAR C Compiler
+#define FLASH_DECLARE(x) __flash x
+#endif
+#if defined(__GNUC__) // GNU Compiler
+#define FLASH_DECLARE(x) x __attribute__((__progmem__))
+#endif 
+#define LEFT_BRACE  true
+#define RIGHT_BRACE false
+#define ISSTRING    true
+#define ISNOTSTRING false
+
 // Calcula o HASH DJB 
 #define HASH_DJB(START, LEN, INPUT, OUTPUT) for(i = START; i < LEN; i++){ OUTPUT = ((OUTPUT << 5) + OUTPUT) + INPUT[i]; }
-// Calcula o comprimento da string
-#define STRLEN(STRING,LENGTH) for(LENGTH = 0; STRING[LENGTH] != '\0';){LENGTH++;}
 // Copiar uma string para a outra
-#define STRCPY(INPUT, OUTPUT) do{ for(i = 0;INPUT[i] != 0; ++i) OUTPUT[i] = INPUT[i]; }while(0)
+#define STRCPY(INPUT, OUTPUT) do{ for(i = 0;INPUT[i] != 0; ++i) OUTPUT[i] = INPUT[i]; OUTPUT[i] = 0; }while(0)
+// Gera o body tendo o OBJETO dispositivo
+#define generatePost(DEVICE) do{ DEVICE.generateHeader(); DEVICE.generateBody(); }while(0)
 
 class TATUDevice{
 private:
@@ -31,6 +43,11 @@ private:
         char     value[6];
         uint32_t hash_value;
     } PinStruct;
+
+    void put_braces(char *brace_place, bool direction);
+    void put_colon(char *colon_place, bool string);
+    void put_comma(char *comma_place, bool string);
+    void put_colon_braces(char *brace_place);
 public:
     // Atributos públicos
     // Atributos do sistema
@@ -54,11 +71,14 @@ public:
 
     // Mensagem de saida
     char output_msg[200] = {0};
+    char last_char = 0;
 
     // Methodos públicos
-    TATUDevice();
+    TATUDevice( const char *,  const char *, const uint8_t,  const uint8_t,
+                const uint8_t, const char *, const uint16_t, const uint8_t));
     void generateHeader();
     void generateBody();
+    void generatePost();
 };
 
 #endif
