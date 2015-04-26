6319aa06b99b180795a5cdb0d216c54b0d3f3297
jefersonla <oi_je@hotmail.com>
2015-04-26 14:14:47 -0300

    Concertado pequenos bugs

--- TATUInterpreter/TATUInterpreter.cpp
+++ TATUInterpreter/TATUInterpreter.cpp
@@ -1,74 +1,83 @@
 #include "TATUInterpreter.h"
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
-bool TATUInterpreter::parse(unsigned char *string, unsigned int length){
+uint8_t atoi_T(char *p){
+    int k = 0;
+    while(*p){k = (k << 3) + (k << 1) + (*p) - '0'; p++; }
+    return k;
+}
+
+bool TATUInterpreter::parse(char *string, unsigned int length){
     uint8_t i, j = 0;
     str_hash = 5381;
+    cmd.STRUCTURE = 0;
     cmd.OBJ.VAR = TATU_TYPE_ALIAS;
 
     switch(string[0]){
         case COMMAND_SET:
             cmd.OBJ.TYPE = TATU_SET;
-            if(string[4] == CODE_INFO){
-                cmd.OBJ.CODE = TATU_CODE_INFO;
-                j = 9;
-
-            }
-            else if(string[4] == CODE_STATE){
-                cmd.OBJ.CODE = TATU_CODE_STATE;
-                j = 10; 
-            }
-            else if(string[4] == CODE_VALUE){
-                cmd.OBJ.CODE = TATU_CODE_VALUE;
-                j = 10; 
+            j = 9;
+            switch(string[4]){
+                case CODE_INFO:
+                    cmd.OBJ.CODE = TATU_CODE_INFO;
+                    break;
+                case CODE_STATE:
+                    cmd.OBJ.CODE = TATU_CODE_STATE;
+                    j++;
+                    break;
+                case CODE_VALUE:
+                    cmd.OBJ.CODE = TATU_CODE_VALUE;
+                    j++;
+                    break;
             }
             break;
         case COMMAND_EDIT:
             cmd.OBJ.TYPE = TATU_EDIT;
-
-            if(string[5] == CODE_INFO){
-                cmd.OBJ.CODE = TATU_CODE_INFO;
-                j = 10; 
-
-            }
-            else if(string[5] == CODE_STATE){
-                cmd.OBJ.CODE = TATU_CODE_STATE;
-                j = 11; 
-            }
-            else if(string[4] == CODE_VALUE){
-                cmd.OBJ.CODE = TATU_CODE_VALUE;
-                j = 11; 
+            j = 10;
+            switch(string[5]){
+                case CODE_INFO:
+                    cmd.OBJ.CODE = TATU_CODE_INFO;
+                    break;
+                case CODE_STATE:
+                    cmd.OBJ.CODE = TATU_CODE_STATE;
+                    j++; 
+                    break;
+                case CODE_VALUE:
+                    cmd.OBJ.CODE = TATU_CODE_VALUE;
+                    j++; 
             }
             break;
         case COMMAND_GET:
             cmd.OBJ.TYPE = TATU_GET;
+            j = 9;
             
-            if(string[4] == CODE_ALL){
-                j = 8;
-                cmd.OBJ.CODE = TATU_CODE_ALL;
-                return true;
-            }
-            else if(string[4] == CODE_INFO){
-                cmd.OBJ.CODE = TATU_CODE_INFO;
-                j = 9; 
-            }
-            else if(string[4] == CODE_STATE){
-                cmd.OBJ.CODE = TATU_CODE_STATE;
-                j = 9; 
-
-            }
-            else if(string[4] == CODE_VALUE){
-                cmd.OBJ.CODE = TATU_CODE_VALUE;
-                j = 10; 
+            switch(string[4]){
+                case CODE_ALL:
+                    cmd.OBJ.CODE = TATU_CODE_ALL;
+                    return true;
+                case CODE_INFO:
+                    cmd.OBJ.CODE = TATU_CODE_INFO;
+                    break; 
+                case CODE_STATE:
+                    cmd.OBJ.CODE = TATU_CODE_STATE;
+                    break; 
+                case CODE_VALUE:
+                    cmd.OBJ.CODE = TATU_CODE_VALUE;
+                    j++;
+                   break; 
             }
             break;
-
+        case COMMAND_POST:
+            for(j = 6; string[j] != ':'; j++);
+            strcpy(payload, &payload[++j]);
+            return true;
     }
 
-    if (!j)
-        return false;
+    if (!j) return false;
 
-    if(IS_NUM(string[j])){ cmd.OBJ.VAR = TATU_TYPE_PIN; cmd.PIN = (uint8_t)atoi(&string[strlen(string)+1]);}
+    if(IS_NUM(string[j])){ cmd.OBJ.VAR = TATU_TYPE_PIN; cmd.OBJ.PIN = atoi_T(&string[strlen(string)+1]);}
     else if(IS_SYS(string[j])){ j++; cmd.OBJ.VAR = TATU_TYPE_SYSTEM;}
 
     HASH_DJB(j, length, string, str_hash);
--- TATUInterpreter/TATUInterpreter.h
+++ TATUInterpreter/TATUInterpreter.h
@@ -2,8 +2,6 @@
 #define TATUInterpreter_h
 
 #include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
 
 // TATU Protocol available commands
 #define TATU_POST   0
@@ -21,7 +19,7 @@
 #define TATU_TYPE_ALIAS  0
 #define TATU_TYPE_PIN    1
 #define TATU_TYPE_SYSTEM 2
-
+#define TATU_TYPE_CRYPTO 3
 
 // Char that represents the TATU Protocol commands
 #define CODE_ALL   'A'
@@ -35,14 +33,17 @@
 #define COMMAND_GET  'G'
 #define COMMAND_EDIT 'E'
 
-
-// DJB Hash macroUE
+// DJB Hash macro
 #define HASH_DJB(START, LEN, INPUT, OUTPUT) for(i = START; INPUT[i] != ' '; i++){ OUTPUT = ((OUTPUT << 5) + OUTPUT) + INPUT[i]; }
 // Copy property to begin of the payload array
 #define VAR_COPY(START, LEN, VECTOR) for(i = START; i < LEN; i++){ VECTOR[i-START] = VECTOR[i]; if(i+1 == LEN) VECTOR[i-START+1] = '\0'; else if(VECTOR[i] == ' ') VECTOR[i] = 0; }
 // Verifica se uma variavel é um numero
 #define IS_NUM(VAR) (VAR < 58)&&(VAR > 47)
 #define IS_SYS(VAR) (VAR == '$')
+#define IS_CRYPTO(VAR) (VAR == '#')
+
+/* Utilities */
+uint8_t atoi_T(char *p);
 
 class TATUInterpreter{
 private:
@@ -53,16 +54,15 @@ private:
             uint8_t CODE  : 2;
             uint8_t TYPE  : 2;
             uint8_t VAR   : 2;
+            uint8_t PIN      ;
         } OBJ;
-        uint8_t PIN;
         uint16_t STRUCTURE;
     } Command;
-
 public:
     Command cmd;
     uint32_t str_hash;
     TATUInterpreter(){ cmd.OBJ.ERROR = true; }
-    bool parse(unsigned char *, unsigned int);
+    bool parse(char *, unsigned int);
 };
 
 #endif
