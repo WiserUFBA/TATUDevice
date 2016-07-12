ac82a9ef916cd347230e08fd8c38d85e130aeb76
Jeferson Lima <oi_je@hotmail.com>
2016-07-12 20:49:04 -0300

    Update sensors.h

--- sensors/sensors.h
+++ sensors/sensors.h
@@ -13,13 +13,45 @@ int aux;
 #endif
 
 
-//#define v_out(PIN)\ 
+//#define v_out(PIN)\
 //  analogRead(PIN)*5.0/1024.0
 
 /*#define lux(RESL,PIN)\
     do{\
       ((2500/analogRead(PIN)*5.0/1024.0) - 500)/RESL;\
     }while(false)*/
+    /*Irms = emon1.calcIrms(1480);\
+    aux1 = Irms*100;\
+    VAR = (int)Irms;\
+    VAR = VAR*100;\
+    aux1 = aux1 - 100;\
+    VAR = VAR/100;\
+    ITOS(VAR,aux20);\
+    ITOS(aux1,aux10);\
+    switch(CODE){\
+      case TATU_CODE_INFO:\
+        strcpy(RESPONSE,aux20);\
+        RESPONSE[strlen(aux20)] = '.';\
+        RESPONSE[strlen(aux20)+1] = '\0';\
+        strcpy(RESPONSE[strlen(RESPONSE)],aux10);\
+        aux = strlen((char*)RESPONSE);\
+        ((char*)RESPONSE)[aux++] = 'A';\
+        ((char*)RESPONSE)[aux] = 0;\*/
+ #define current_sensor(PIN,VAR,RESPONSE,CODE)\
+          do{\
+    		    Irms = emon1.calcIrms(1480);\
+            switch(CODE){\
+              case TATU_CODE_INFO:\
+                /*aparentemente só precisa disso*/\
+                dtostrf(Irms,2,2,(char *)RESPONSE);\
+                break;\
+              case TATU_CODE_VALUE:\
+                ITOI(VAR,RESPONSE);\
+                break;\
+              default:\
+                  return false;\
+            }\
+          }while(false)
 
 #define gas_sensor(PIN,VAR,RESPONSE,CODE)\
       do{\
@@ -49,7 +81,7 @@ int aux;
 #define luminosity_sensor(PIN,VAR,RESPONSE,CODE,RESL)\
       do{\
         v_out = ( (float)analogRead(PIN)*(5.0/1024.0));\
-        luz = ( ( 2500 / v_out ) - 400)/RESL;\ 
+        luz = ( ( 2500 / v_out ) - 400)/RESL;\
         /*Serial.print("v_out ");*/\
         /*Serial.println(v_out);*/\
         /*Serial.print("luz ");*/\
@@ -75,7 +107,7 @@ int aux;
 #define dht_temperature_sensor(DHT,VAR,RESPONSE,CODE)\
       do{\
         VAR = (int)DHT.readTemperature();\
-        switch(CODE){\ 
+        switch(CODE){\
           case TATU_CODE_INFO:\
             ITOS(VAR,RESPONSE);\
             break;\
@@ -84,12 +116,12 @@ int aux;
             break;\
           default:\
             return false;\
-        }\ 
+        }\
       }while(false)
 #define dht_humidity_sensor(DHT,VAR,RESPONSE,CODE)\
       do{\
         VAR = (int)DHT.readHumidity();\
-        switch(CODE){\ 
+        switch(CODE){\
           case TATU_CODE_INFO:\
             ITOS(VAR,RESPONSE);\
             break;\
@@ -103,7 +135,7 @@ int aux;
 #define sensor(PIN,VAR,RESPONSE,CODE)\
       do{\
         VAR = analogRead(PIN);\
-        switch(CODE){\ 
+        switch(CODE){\
           case TATU_CODE_INFO:\
             ITOS(VAR,RESPONSE);\
             break;\
@@ -116,7 +148,7 @@ int aux;
       }while(false)
 #define att_sensor(VAR,RESPONSE,CODE)\
       do{\
-        switch(CODE){\ 
+        switch(CODE){\
           case TATU_CODE_INFO:\
             ITOS(VAR,RESPONSE);\
             break;\
@@ -129,7 +161,7 @@ int aux;
       }while(false)
 #define bool_sensor(VAR,RESPONSE,CODE)\
       do{\
-        switch(CODE){\ 
+        switch(CODE){\
           case TATU_CODE_INFO:\
             BTOS(VAR,RESPONSE);\
             break;\
