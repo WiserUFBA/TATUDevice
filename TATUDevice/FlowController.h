ad64237eb1435295d13d1dc30c1fc3e79916c251
ramondcosta <ramon.eumsm.costa@gmail.com>
2016-08-01 17:37:08 -0300

    mais codigo da classe feito

--- TATUDevice/FlowController.h
+++ TATUDevice/FlowController.h
@@ -1,34 +1,41 @@
-#ifndef TATUDevice_h
-#define TATUDevice_h
-typedef struct flowUnit {
-  unsigned long int collect_freq, publish_freq;
-  void* vector; uint8_t iterator, size;
-  void* publish_method;
-  void* message;
-  uint32_t att, flow;
-  flowUnit* next;
-  int lastTimeCollect, lastTimePub;
-  uint8_t type;
-  bool used;
-} flowUnit, *flowList;
+#ifndef FlowController_h
+#define FlowController_h
+
+#include <stdint.h>
+
+//#define flowList *FlowUnit
+
+
+class FlowUnit {
+  public:
+    unsigned long int collect_freq, publish_freq;
+    void* vector; uint8_t iterator, size;
+    void* publish_method;
+    void* message;
+    uint32_t att, flow;
+    FlowUnit* next;
+    int lastTimeCollect, lastTimePub;
+    uint8_t type;
+    bool used;
+};
+typedef FlowUnit* FlowList;
 
 class FlowController{
 public:
-    flowList activity;
-
+    FlowUnit activity;
 
     FlowController();
-    void buffer_alloc(uint8_t size, flowList unit);
+    void buffer_alloc(uint8_t size, FlowList unit);
     void flowbuilder(char* json, uint32_t hash, uint8_t code);
-    void* vector_iterator(flowList unit);
-    void flow_loop(flowList unit);
+    void* vector_iterator(FlowList unit);
+    void flow_loop(FlowList unit);
     void requisition(void* response, uint32_t hash);
-    void* vector_acess(flowList unit, int i);
-    void push_value(char* response, flowList unit, int i);
-    void flow_publish(flowList unit);
-    void flow_construct(uint32_t hash, int collect_freq, void* message, int publish_freq, uint8_t code, uint8_t type, void* vector, uint32_t flow, flowList unit);
+    void* vector_acess(FlowList unit, int i);
+    void push_value(char* response, FlowList unit, int i);
+    void flow_publish(FlowList unit);
+    void flow_construct(uint32_t hash, int collect_freq, void* message, int publish_freq, uint8_t code, uint8_t type, void* vector, uint32_t flow, FlowList unit);
 
 private:
-    //flowList unit;
+    //FlowList unit;
 };
 #endif
